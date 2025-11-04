8e58185e8521f670a3f21b52334201a80d670584
Jeongbin Cho/조정빈 <165370817+wjdbin@users.noreply.github.com>
2025-11-04 22:40:19 +0900

    Create code.cpp

---
+++ 11382/code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// 10^12 (1조) 단위의 정수를 다루기 위해 long long 사용
+	long long a, b, c; 
+
+	cin >> a >> b >> c;
+	cout << a + b + c;
+
+	return 0;
+}
